f5eb4390065a7119b17be6072bb3e27705898d1d
Federico Marchionni <federico.marchionni@caseware.com>
2026-05-02 22:07:50 +0100

    Set Android navigation bar to dark grey

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as NavigationBar from 'expo-navigation-bar';
 import { RootNavigator } from '@/navigation/RootNavigator';
 
 // Initialise Firebase (side effect import)
 import '@/services/firebase/config';
 
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setBackgroundColorAsync('#2B2B2B');
+      NavigationBar.setButtonStyleAsync('light');
+    }
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
